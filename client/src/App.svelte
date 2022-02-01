bf8786468a7f6132006e751526bffeca5fdf9ccb
HttpRafa <rafael.kienitz@gmail.com>
2022-02-01 18:55:36 +0100

    Worked on the client

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+    import SideBar from "./sidebar/SideBar.svelte";
+    import HomeContent from "./components/HomeContent.svelte";
 </script>
 
-<main>
-
+<main class="flex">
+    <SideBar />
+    <HomeContent />
 </main>
 
 <style>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,40 @@
 const production = !process.env.ROLLUP_WATCH;
 module.exports = {
+    enabled: true,
     future: {
         purgeLayersByDefault: true,
         removeDeprecatedGapUtilities: true,
     },
-    plugins: [
-    ],
+    mode: 'jit',
     purge: {
         content: [
-            "./src/App.svelte",
+            "./src/**/*.svelte",
         ],
         enabled: production // disable purge in dev
     },
+    darkMode: 'class', // class, 'media' or boolean
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    900: '#202225',
+                    850: '#242629',
+                    800: '#2f3136',
+                    700: '#36393f',
+                    600: '#4f545c',
+                    400: '#d4d7dc',
+                    300: '#e3e5e8',
+                    200: '#ebedef',
+                    100: '#f2f3f5',
+                },
+                orange: {
+                    450: '#f77a22'
+                },
+            },
+            spacing: {
+                88: '22rem',
+            },
+        },
+    },
+    plugins: [],
 };
