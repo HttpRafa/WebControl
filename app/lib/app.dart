cf6b3eb8692094b0075d7cd686bfafb124946498
HttpRafa <rafadevmc@gmail.com>
2022-02-11 16:20:05 +0100

    Worked on the app

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -1,32 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:webcontrol/screens/location_detail/location_detail.dart';
 
 class App extends StatelessWidget {
-
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const LocationDetail(),
-      theme: ThemeData(
-        // Define the default brightness and colors.
-        brightness: Brightness.light,
-        primaryColor: Colors.lightBlue[800],
-
-        // Define the default font family.
-        fontFamily: 'Montserrat',
-
-        // Define the default `TextTheme`. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
-        textTheme: const TextTheme(
-          headline1: TextStyle(fontSize: 30.0, fontWeight: FontWeight.normal),
-          headline6: TextStyle(fontSize: 20.0, fontStyle: FontStyle.normal),
-          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Montserrat'),
-          bodyText1: TextStyle(fontSize: 18.0, fontFamily: 'Montserrat'),
-        ),
+        home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text("Test"),
       ),
-    );
+      body: const Text("Test"),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Test"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Test"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Test"),
+        ],
+      ),
+      drawer: const Drawer(
+        child: Text("A Drawer"),
+      ),
+    ));
   }
-
 }
